ac1b10ccbc947e758db29f4c1c0eed8b375e4b00
Tim Visser <Tim.Visser6@hva.nl>
2017-11-21 14:52:25 +0100

    test.c toegevoegd

--- KernelModuleRPI/src/suzanne_tim.c
+++ KernelModuleRPI/src/suzanne_tim.c
@@ -1,16 +1,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <wiringPi.h>
+//#include <wiringPi.h>
 
-#define GPIO_LED_PIN 21
+//#define GPIO_LED_PIN 21
 
 int hello_init(void) {
 	pr_alert("Character Device van Suzanne Peerdeman & Tim Visser\n");
-	if (wiringPiSetup () == -1) {
-		pr_alert("Wiring Pi is niet geinstalleerd!");
-		return 1;
-	}
-	pinMode(GPIO_LED_PIN, OUTPUT);
+//	if (wiringPiSetup () == -1) {
+//		pr_alert("Wiring Pi is niet geinstalleerd!");
+//		return 1;
+//	}
+//	pinMode(GPIO_LED_PIN, OUTPUT);
 	return 0;
 }
 
@@ -20,12 +20,12 @@ void hello_exit(void) {
 
 static void LED_aan(void) {
 	pr_alert("LEDje aan...\n");
-	digitalWrite(GPIO_LED_PIN, HIGH);
+//	digitalWrite(GPIO_LED_PIN, HIGH);
 }
 
 static void LED_uit(void) {
 	pr_alert("LEDje uit...\n");
-	digitalWrite(GPIO_LED_PIN, LOW);
+//	digitalWrite(GPIO_LED_PIN, LOW);
 }
 
 module_init(hello_init);
---
+++ KernelModuleRPI/src/test.c
@@ -0,0 +1,14 @@
+#include <suzanne_tim.c>
+#include <unistd.h>
+
+int main (int argc, char **argv) {
+
+    while (1) {
+        LED_aan();
+        usleep(1000000);
+        LED_uit();
+        usleep(1000000);
+    }
+
+    return 0;
+}
